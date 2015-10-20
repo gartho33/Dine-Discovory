664b52bdf2b2bfb4ca73158fc7e1b3ee91c81d71
kstclaire95 <kstclaire95@gmail.com>
2015-10-20 08:59:54 -0600

    Added map marker and bounce animation

--- Assets/JS/initMap.js
+++ Assets/JS/initMap.js
@@ -5,13 +5,21 @@ function initialize(){
         zoom: 8,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     }
-    var map = new google.maps.Map(mapCanvas, mapOptions)
-    
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+   // var marker = new google.maps.Marker({ position: map.center, });
+
     if(navigator.geolocation){
         browserSupportFlag = true;
         navigator.geolocation.getCurrentPosition(function(position) {
           initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
-        map.setCenter(initialLocation);
+            map.setCenter(initialLocation);
+
+            var marker = new google.maps.Marker({
+                position: map.center,
+                animation: google.maps.Animation.BOUNCE,
+            });
+            marker.setMap(map);
+            setTimeout(function(){ marker.setAnimation(null); }, 1400);
         }, function(){
             handleNoGeolocation(browserSupportFlag);
         });
