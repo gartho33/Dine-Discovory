7150bf89fc6bd068a5202bc8d6c1459ad82f301b
Neal Byington <garth033@hotmail.com>
2015-10-20 09:40:25 -0600

    working on querying google maps

--- Assets/JS/initMap.js
+++ Assets/JS/initMap.js
@@ -1,8 +1,9 @@
+var browserAPIKey = "AIzaSyBFOLAH3N-pqX-S63W_97qKbvgt0TBW1ek";
+
 function initialize(){
     var mapCanvas = document.getElementById('map');
     var mapOptions = {
-        center: new google.maps.LatLng(44.5403, -78.5463),
-        zoom: 8,
+        zoom: 15,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     }
     var map = new google.maps.Map(mapCanvas, mapOptions)
@@ -19,15 +20,31 @@ function initialize(){
         browserSupportFlag = false;
         handleNoGeolocation(browserSupportFlag);
     }
+    
+    var newyork = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
     function handleNoGeolocation(errorFlag) {
-        if (errorFlag == true) {
-          alert("Geolocation service failed.");
-          initialLocation = newyork;
-        } else {
-          alert("Your browser doesn't support geolocation. We've placed you in Siberia.");
-          initialLocation = siberia;
+            if (errorFlag == true) {
+              alert("Geolocation service failed.");
+              initialLocation = newyork;
+            } else {
+              alert("Your browser doesn't support geolocation. We've placed you in Newyork.");
+              initialLocation = newyork;
+            }
+            map.setCenter(initialLocation);
+      }
+    
+    var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch({
+        location: map.center,
+        radius: 500,
+        types: ['store']
+    }, callback);
+    
+    function callback(results, status){
+        if(status === google.maps.places.PlacesServiceStatus.OK){
+            $('output').text = results;
+            console.log(results);
         }
-        map.setCenter(initialLocation);
-  }
+    }
 }
 google.maps.event.addDomListener(window, 'load', initialize);
